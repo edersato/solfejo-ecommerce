3ea8c3c1d4e2d483d1e1e5eff4dc95c6f9334d80
edersato <edersato@gmail.com>
2023-06-03 20:31:37 -0300

    Inclusão do footer e tela de contato

--- src/main.js
+++ src/main.js
@@ -2,11 +2,24 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import { BootstrapVue } from 'bootstrap-vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+import HomeIndex from './components/home/HomeIndex'
+import ProductIndex from './components/product/ProductIndex'
+import CartIndex from './components/cart/CartIndex'
+import AboutIndex from './components/about/AboutIndex'
+
+Vue.component('HomeIndex', HomeIndex)
+Vue.component('ProductIndex', ProductIndex)
+Vue.component('CartIndex', CartIndex)
+Vue.component('AboutIndex', AboutIndex)
+
+
+
 Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false
 
