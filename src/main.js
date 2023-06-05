c3958dd693893e6f654796a4e175aaa33ace65b1
edersato <edersato@gmail.com>
2023-06-04 22:28:55 -0300

    last changes on code

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -23,6 +22,5 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
-  store,
   render: function (h) { return h(App) }
 }).$mount('#app')
